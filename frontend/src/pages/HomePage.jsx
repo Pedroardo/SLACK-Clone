66c0ac4c5161af95d16375df8f1563a2d1c38bf4
Pedro <pedroardo10@gmail.com>
2025-09-03 10:37:35 +0700

    auth page on frontend implemented

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { SignOutButton } from "@clerk/clerk-react";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <SignOutButton>Sign Out</SignOutButton>
+    </div>
+  );
+};
+
+export default HomePage;
