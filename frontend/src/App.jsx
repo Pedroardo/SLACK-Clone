a102c38924a3f4200dc26b50d659ebc08723731f
Pedro <pedroardo10@gmail.com>
2025-09-04 11:27:47 +0700

    adding useStreamChat hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,65 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-import { Routes, Route, Navigate } from "react-router";
-import HomePage from "./pages/HomePage";
-import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/clerk-react";
 import * as Sentry from "@sentry/react";
+import { Navigate, Route, Routes } from "react-router";
+import AuthPage from "./pages/AuthPage";
+import CallPage from "./pages/CallPage";
+import HomePage from "./pages/HomePage";
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 function App() {
-  const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return null;
+
   return (
     <>
-      <SignedIn>
-        <SentryRoutes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<Navigate to={"/"} replace />} />
-        </SentryRoutes>
-      </SignedIn>
+      <SentryRoutes>
+        <Route
+          path="/"
+          element={
+            isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />
+          }
+        />
+        <Route
+          path="/auth"
+          element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+        />
 
-      <SignedOut>
-        <SentryRoutes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<Navigate to={"/auth"} replace />} />
-        </SentryRoutes>
-      </SignedOut>
+        <Route
+          path="/call/:id"
+          element={
+            isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            isSignedIn ? (
+              <Navigate to={"/"} replace />
+            ) : (
+              <Navigate to={"/auth"} replace />
+            )
+          }
+        />
+      </SentryRoutes>
     </>
   );
 }
 
 export default App;
+
+//  <>
+//       <SignedIn>
+//         <SentryRoutes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/auth" element={<Navigate to={"/"} replace />} />
+//         </SentryRoutes>
+//       </SignedIn>
+
+//       <SignedOut>
+//         <SentryRoutes>
+//           <Route path="/auth" element={<AuthPage />} />
+//           <Route path="*" element={<Navigate to={"/auth"} replace />} />
+//         </SentryRoutes>
+//       </SignedOut>
+//     </>
